e14e412e223e589548b5921ee5c19f50dc04a835
Tim Petter <tim@timpetter.de>
2015-09-23 16:20:24 +0200

    Cleanup code

--- stars/stars.go
+++ stars/stars.go
@@ -2,6 +2,7 @@ package stars
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -11,14 +12,13 @@ import (
 )
 
 const (
-	apiEnpoint = "https://api.github.com/users/"
-	apiPath    = "/starred"
+	apiPath = "https://api.github.com/users/%v/starred"
 )
 
 // Stars …
 type Stars struct {
 	URL      *url.URL
-	Pages    int64
+	Pages    int
 	Username string
 }
 
@@ -36,6 +36,10 @@ func (s *Stars) Repos() ([]StaredRepos, error) {
 	s.Username = strings.SplitN(s.URL.Path, "/", 3)[1]
 	log.Printf("%v\n", s.Username)
 
+	if err := s.setPagesCount(); err != nil {
+		return nil, err
+	}
+
 	r, err := s.starsFromPage(1)
 	if err != nil {
 		return nil, err
@@ -45,20 +49,33 @@ func (s *Stars) Repos() ([]StaredRepos, error) {
 	return repos, nil
 }
 
-func (s *Stars) starsFromPage(p int64) ([]StaredRepos, error) {
-	var repos []StaredRepos
-
-	apiURL := apiEnpoint + s.Username + apiPath + "?page=" + strconv.Itoa(int(p))
-
+func (s *Stars) setPagesCount() error {
+	apiURL := fmt.Sprintf(apiPath, s.Username)
 	log.Printf("%v\n", apiURL)
+
 	resp, err := http.Get(apiURL)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 
 	s.Pages, err = pagesCount(resp.Header.Get("Link"))
 	log.Printf("%v\n", s.Pages)
+
+	return nil
+}
+
+func (s *Stars) starsFromPage(p int) ([]StaredRepos, error) {
+	var repos []StaredRepos
+
+	apiURL := fmt.Sprintf(apiPath+"?page=%v", s.Username, strconv.Itoa(p))
+	log.Printf("%v\n", apiURL)
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
 	if err := json.NewDecoder(resp.Body).Decode(&repos); err != nil {
 		log.Printf("%v\n", err)
@@ -68,19 +85,22 @@ func (s *Stars) starsFromPage(p int64) ([]StaredRepos, error) {
 	return repos, nil
 }
 
-func pagesCount(s string) (int64, error) {
+func pagesCount(s string) (int, error) {
+	if s == "" {
+		return 1, nil
+	}
 	re, err := regexp.Compile("page=\\d+")
 	if err != nil {
 		return 0, err
 	}
-	found := re.FindAllString(s, -1)
+	found := re.FindAllString(s, -1)[1]
 
 	re2, err := regexp.Compile("\\d+")
 	if err != nil {
 		return 0, err
 	}
 
-	i, err := strconv.ParseInt(re2.FindString(found[1]), 10, 64)
+	i, err := strconv.Atoi(re2.FindString(found))
 	if err != nil {
 		return 0, err
 	}
